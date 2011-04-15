5d89c3929fdf2ac18e14917a98708ae34ad6e818
bruno_bl <bruno_bl@yahoo.com.br>
2011-04-15 00:51:47 +0000

    Use parameters to define solver type (AMG,CG,GMRES, etc.)

--- Delfine/Src/SolveEqSystem.py
+++ Delfine/Src/SolveEqSystem.py
@@ -13,10 +13,9 @@
 #############################################################
 from scipy.sparse import csr_matrix
 from pyamg import smoothed_aggregation_solver
-
-# For tests only###############
 from pyamg.krylov._cg import cg
-#########################
+from pyamg.krylov._gmres import gmres
+import sys
 
 class SolveEqSystem:
     """Solves the equation system with pyAMG.""" 
@@ -24,11 +23,23 @@ class SolveEqSystem:
     def __init__(self):
         pass
     
-    def solve_withPyAMG(self, delfineVar):
+    def solve_withPyAMG(self, delfineVar, parameter):
         """Solves the equation system with pyAMG.
             Input:
             elliptic = AssembleElliptic() [AssembleElliptic.py]
         """
+        
+        # Reads  solver data
+        # Alternatives: CG (with or without PreCond) | GMRES (w/ or w/o PC)
+        #                     | LU (still pending))
+        solverType = parameter.num.pressSolv.type
+        tolerance = parameter.num.pressSolv.tolerance
+        maxStep = parameter.num.pressSolv.maxNumSteps
+        
+        # Reads type of preconditioner
+        # Alternatives: AMG | ILU | None
+        preCondType = parameter.num.pressSolv.preConditioning.type
+        
         
         # Getting data from elliptic eq. assembler
         A = delfineVar.A
@@ -37,6 +48,7 @@ class SolveEqSystem:
         # Get sparse matrix data
         (row,col,data) = A.data()
         n = A.size(0)
+        
         # It was commented the line 126 and 129 of the file compressed.py
         # of the scipy package located in /usr/local/lib/python2.6/dist-packages
         # /scipy/sparse in order to avoid some annoying warning messages.
@@ -46,26 +58,51 @@ class SolveEqSystem:
         
         # Get right-hand side vector(rhs) data
         b = rhs.data()
+        residuals = []
         
         # Solves equation system
-        # OBS: The max_coarse parameters tells the maximum number of Degrees of
-        # freedom in the coarsest mesh, and not the number of coarse levels as would
-        # be expected (at least by me!).
+        if (preCondType == "amg"):
+            # Using AMG Solver as preconditioner with 'solverType' (cg,gmres) as 
+            # accelerator. If ilu is defined as solver, it will use pure AMG without
+            # accelaration.
+            nCoarse = parameter.num.pressSolv.preConditioning.numCoarseLevel
+            ml = smoothed_aggregation_solver(Asp, max_levels=nCoarse, max_coarse=1)
+            if (solverType != "lu"):
+                # Use CG or GMRES acceleration
+                x = ml.solve(b,tol=tolerance, maxiter=maxStep, cycle='V', accel=solverType,residuals=residuals)
+            else:
+                # No accelaration (stand-alone AMG)
+                x = ml.solve(b,tol=tolerance, maxiter=maxStep, cycle='V', residuals=residuals)
+            
+            print ml
         
-        # Using AMG Solver
-        ml = smoothed_aggregation_solver(Asp,max_coarse=1)
-        residuals = []
-        x = ml.solve(b,tol=1e-10,accel="cg",residuals=residuals)
+        elif (preCondType == "none") :
+            # Iterate without preconditioner
+            if (solverType == "cg"):
+                # Using conventional Conjugate Gradients Solver
+                (x, flag) = cg(Asp,b, maxiter=maxStep, tol=tolerance,  residuals=residuals)
+            elif (solverType == "gmres"):
+                # Using conventional Generalized Minimum Residual Method Solver
+                (x, flag) = gmres(Asp,b, maxiter=maxStep, tol=tolerance,  residuals=residuals)
+            elif (solverType == "lu"):
+                # Using a direct LU solver
+                # (still pending, to be done with dolfin solver schema, not pyamg)
+                print "Error(7):"
+                print "Direct solver still not available, use cg or gmres instead"
+                print " "
+                sys.exit(1)
+                
+        elif (preCondType == "ilu"):
+                # Using a ILU preconditioner
+                # (still pending, to be done with dolfin solver schema, not pyamg)
+                print "Error(8):"
+                print "ILU Preconditioner still not available, use amg or none instead"
+                print " "
+                sys.exit(1)
         
-        # Using conventional Conjugate Gradients Solver (use parameter.num to define it
-        # automatically on initialization)
-        #(x, flag) = cg(Asp,b, maxiter=200, tol=1e-10,  residuals=residuals)
-   
         # Print residuals history
         residuals = residuals/residuals[0]
-        print ml
-        ##print residuals
-        
+  
         # Define return parameters
         delfineVar.x = x
         delfineVar.residuals = residuals
