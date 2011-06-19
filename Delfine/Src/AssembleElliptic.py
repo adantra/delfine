723b2f695adf110f8b227794ee538c92976fc546
bruno_bl <bruno_bl@yahoo.com.br>
2011-06-19 15:44:35 +0000

    Dummy para apresentação!! Mudar de volta para original depois!

--- Delfine/Src/AssembleElliptic.py
+++ Delfine/Src/AssembleElliptic.py
@@ -41,31 +41,34 @@ class AssembleElliptic:
             K = PermeabilityTensor2D(mesh, parameter)
         elif (dim == 3):
             K = PermeabilityTensor3D(mesh, parameter)
-        
+
         # Read data for fluid viscosity
         muW = ViscScalar( "water", parameter)
         muO = ViscScalar( "oil", parameter)
-        
+
         # Read data for relative permeabilities
         Sw = 1.0 # FIXME: This comes from sat. solution
         krW = RelatPermScalar("water", parameter, Sw)
         krO = RelatPermScalar("oil", parameter, Sw)
-        
+
         # Define partials and total mobilites
         mobW = krW/muW
         mobO = krO/muO
         mob = mobW + mobO
-        
+
         # Create function space
         V = FunctionSpace(mesh, "CG", order)
-        
+
         # Define Dirichlet boundary conditions
         u0= BCCond()
-        bc = DirichletBC(V, u0, u0_boundary,  "pointwise") 
+        bc = DirichletBC(V, Expression("0.0"), u0_boundary,  "pointwise") 
+#        from DelfineBC import u1_boundary
+#        u1 = BCCond()
+#        bc = DirichletBC(V, Expression("1.0"), u1_boundary,  "pointwise") 
 
         # Define distributed source terms (not wells)
         f = Source()
-        
+
         # Define Neumman boundary conditions
         g = Expression("0.0")
 
@@ -79,11 +82,34 @@ class AssembleElliptic:
         # Assemble matrices and vectors
         A, rhs = assemble_system(a, L)
         bc.apply(A, rhs)
-        
+
         # Apply well source/sink conditions
+        # TEST ---------------------------------------------------
+#        vertexs = vertices(mesh)
+#        wells = parameter.geom.bc.wells
+#        print "Get Wells:"
+#        mf = mesh.data().mesh_function("well_indicators")
+#        if (mf != None):
+#            for vertex in vertexs:
+#                for well in wells:
+#                    if (mf[vertex.index()] == int(well.id)):
+#                        print "Vertex:",  vertex.index()
+#                        print "Vertex Coord:",  vertex.midpoint().x(), vertex.midpoint().y(), vertex.midpoint().z()
+#                        print "Well ID:",  well.id
+#                        print "Well Value:",  float(well.value)
+#                        if (int(well.id) > 300):
+#                            p = Point(vertex.midpoint().x(), vertex.midpoint().y())
+#                            print vertex.midpoint().x(),  type(vertex.midpoint().y())
+#                            q = PointSource(V, p, float(well.value)) 
+#                            print float(well.value)
+#                            q.apply(rhs)
+
+        
         p = Point(0, 0)
-        delta = PointSource(V, p, 1)
-        delta.apply(rhs)
+        q = PointSource(V, p,1.0) 
+        q.apply(rhs)
+        
+        # END TEST ------------------------------------------
 
         # Define return parameters
         delfineVar.A = A
@@ -91,3 +117,21 @@ class AssembleElliptic:
         delfineVar.V = V
         delfineVar.u0 = u0
 ############################################################
+
+
+
+    # Define variational problem
+    V = FunctionSpace(mesh, "Lagrange", order)
+    v = TestFunction(V)
+    p = TrialFunction(V)
+
+    a = inner(grad(v), K*mob*grad(p))*dx
+    L = v*f*dx - g*v*ds
+    A, rhs = assemble_system(a, L) # A*p=rhs
+    
+    
+    # Project velocity vector on vector function space
+    Vg = VectorFunctionSpace(mesh, "Lagrange", order)
+    vT = project(-K*mob*grad(p), Vg) - project(-K*mobW*dPc*grad(Sw), Vg)
+
+
